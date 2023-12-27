074516bb2c0fa535792cd098f889efeb2d53c553
Raul Delfino <rauldossantoss@gmail.com>
2023-12-27 13:35:23 -0300

    Add Person

---
+++ 05_FirstStepsInJavawithSpringBoot/Rest-with-spring-Boot-and-java-erudio/src/main/java/br/com/erudio/model/Person.java
@@ -0,0 +1,79 @@
+package br.com.erudio.model;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+
+public class Person implements Serializable{
+    
+    private static final long serialVersionUID =1L;
+
+    private Long id;
+
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String gender;
+
+
+    public Person() {}
+
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getAddress() {
+        return this.address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getGender() {
+        return this.gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Person)) {
+            return false;
+        }
+        Person person = (Person) o;
+        return Objects.equals(id, person.id) && Objects.equals(firstName, person.firstName) && Objects.equals(lastName, person.lastName) && Objects.equals(address, person.address) && Objects.equals(gender, person.gender);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, firstName, lastName, address, gender);
+    }
+
+}
